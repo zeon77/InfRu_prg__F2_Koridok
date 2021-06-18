b781a8c6c5e1e16ee07904a17703c19b6fb1219a
zeon77 <zeon77@gmail.com>
2021-06-18 10:29:24 +0200

    Add project files.

---
+++ AutóVerseny/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AutóVerseny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
