da8646f180138fb9d0b107ac87aff0c2b527ad1b
zeon77 <zeon77@gmail.com>
2021-06-18 10:47:08 +0200

    5-6. feladat

--- AutóVerseny/Program.cs
+++ AutóVerseny/Program.cs
@@ -21,6 +21,22 @@ namespace AutóVerseny
             Versenyző FF = versenyzők.Single(v => v.Név == "Fürge Ferenc" && v.Pálya == "Gran Prix Circuit" && v.Kör == 3);
             Console.WriteLine($"4. feladat: {FF.Köridő.TotalSeconds} másodperc");
 
+            //5. feladat
+            Console.WriteLine("5. feladat:");
+            Console.Write("\tKérem egy versenyző nevét: ");
+            string név = Console.ReadLine();
+
+            //6. feladat
+            Console.Write("6. feladat: ");
+            if (!versenyzők.Any(v => v.Név == név))
+            {
+                Console.WriteLine("Nincs ilyen versenyző az allományban!");
+            }
+            else
+            {
+                Versenyző v = versenyzők.Where(x => x.Név == név).OrderBy(x => x.Köridő).First();
+                Console.WriteLine($"{v.Pálya}, {v.Köridő}");
+            }
         }
     }
 }
